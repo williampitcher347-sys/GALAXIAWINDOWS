3f5431febd727b4b690488ac8fa2ba06b5cd9ef9
williampitcher347-sys <williampitcher347@gmail.com>
2026-02-01 21:58:46 +0000

    Delete js directory

--- js/boot.js
+++
@@ -1,7 +0,0 @@
-const savedUser = localStorage.getItem("webos_user");
-if (savedUser) startSession(savedUser);
-
-setInterval(() => {
-  document.getElementById("clock").textContent =
-    new Date().toLocaleTimeString();
-}, 1000);
